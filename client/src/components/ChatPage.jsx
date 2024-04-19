571eb1051542f9999bd22764edbf0f6dbd475762
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-04-19 18:51:38 +0530

    Feat-Core>>Room

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -5,102 +5,95 @@ import { Button } from "./ui/button";
 import { IconMicrophone } from "./IconMicrophone";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
 import { useRecordVoice } from "@/hooks/useRecordVoice";
-import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 
-const ChatPage = ({ socket, username }) => {
-  const [messages, setMessages] = useState([]);
-  const [messageInput, setMessageInput] = useState("");
-  const [isRecording, setIsRecording] = useState(false);
+const ChatPage = ({ socket, username, room }) => {
+	const [messages, setMessages] = useState([]);
+	const [messageInput, setMessageInput] = useState("");
+	const [isRecording, setIsRecording] = useState(false);
 
-  const { startRecording, stopRecording, text } = useRecordVoice();
+	const { startRecording, stopRecording, text } = useRecordVoice();
 
-  useEffect(() => {
-    setMessageInput(text);
-  }, [text]);
+	useEffect(() => {
+		setMessageInput(text);
+	}, [text]);
 
-  useEffect(() => {
-    socket.on("receive-message", (message) => {
-      setMessages([...messages, message]);
-    });
-  }, [messages, socket]);
+	useEffect(() => {
+		if (socket) {
+			socket.emit("join-room", room);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (messageInput == "") {
-      return;
-    }
-    const makeId = () => {
-      return (
-        Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15)
-      );
-    };
-    const messageData = {
-      message: messageInput,
-      username: username,
-      time: `${new Date(Date.now()).getHours()}:${new Date(
-        Date.now()
-      ).getMinutes()}`,
-      id: makeId(),
-    };
+			socket.on("receive-message", (message) => {
+				setMessages((prevMessages) => [...prevMessages, message]);
+			});
 
-    socket.emit("send-message", messageData);
-    setMessageInput("");
-  };
+			return () => {
+				socket.off("receive-message");
+			};
+		}
+	}, [socket, room]);
 
-  const handleStartRecording = () => {
-    startRecording();
-    setIsRecording(true);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (messageInput == "") {
+			return;
+		}
+		const makeId = () => {
+			return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+		};
+		const messageData = {
+			message: messageInput,
+			username: username,
+			time: `${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}`,
+			id: makeId(),
+		};
 
-  const handleStopRecording = () => {
-    stopRecording();
-    setIsRecording(false);
-  };
+		socket.emit("send-message", messageData);
+		setMessageInput("");
+	};
 
-  return (
-    <div className="rounded-md p-2 w-full md:w-[80vw] lg:w-[40vw]">
-      <h1 className="text-center font-bold text-2xl my-2 uppercase">
-        {`${username}'s Chat Room`}
-      </h1>
+	const handleStartRecording = () => {
+		startRecording();
+		setIsRecording(true);
+	};
 
-      <ChatBox messages={messages} username={username} />
-      <form
-        className="flex gap-2 md:gap-4 justify-between "
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          placeholder="Enter message"
-          value={messageInput}
-          onChange={(e) => {
-            setMessageInput(e.target.value);
-          }}
-          className="w-full px-3 py-2  border-2 outline-none rounded-md"
-        />
-        <div className="flex flex-col justify-center items-center">
-          <Button
-            onMouseDown={handleStartRecording}
-            onMouseUp={handleStopRecording}
-            onTouchStart={handleStartRecording}
-            onTouchEnd={handleStopRecording}
-            className={`border-none bg-transparent w-10 p-3 rounded-full hover:bg-gray-500 ${
-              isRecording &&
-              "bg-red-700 text-white px-3 py-2 rounded-md hover:bg-red-700"
-            }`}
-          >
-            <IconMicrophone className="font-bold text-2xl" />
-          </Button>
-        </div>
-        <Button
-          type="submit"
-          className=" font-bold px-3 py-2 bg-gray-500 text-white rounded-md"
-        >
-          <PaperPlaneIcon />
-        </Button>
-      </form>
-    </div>
-  );
+	const handleStopRecording = () => {
+		stopRecording();
+		setIsRecording(false);
+	};
+
+	return (
+		<div className="rounded-md p-2 w-full md:w-[80vw] lg:w-[40vw]">
+			<h1 className="text-center font-bold text-2xl my-2 uppercase">{`${room} Chat Room`}</h1>
+
+			<ChatBox messages={messages} username={username} />
+			<form className="flex gap-2 md:gap-4 justify-between " onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Enter message"
+					value={messageInput}
+					onChange={(e) => {
+						setMessageInput(e.target.value);
+					}}
+					className="w-full px-3 py-2  border-2 outline-none rounded-md"
+				/>
+				<div className="flex flex-col justify-center items-center">
+					<Button
+						onMouseDown={handleStartRecording}
+						onMouseUp={handleStopRecording}
+						onTouchStart={handleStartRecording}
+						onTouchEnd={handleStopRecording}
+						className={`border-none bg-transparent w-10 p-3 rounded-full hover:bg-gray-500 ${
+							isRecording && "bg-red-700 text-white px-3 py-2 rounded-md hover:bg-red-700"
+						}`}
+					>
+						<IconMicrophone className="font-bold text-2xl" />
+					</Button>
+				</div>
+				<Button type="submit" className=" font-bold px-3 py-2 bg-gray-500 text-white rounded-md">
+					<PaperPlaneIcon />
+				</Button>
+			</form>
+		</div>
+	);
 };
 
 export default ChatPage;
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -7,40 +7,55 @@ import { Button } from "./ui/button";
 const socket = io.connect(process.env.NEXT_PUBLIC_API_URL);
 
 const HomePage = () => {
-  const [username, setUsername] = useState("");
-  const [chatActive, setChatActive] = useState(false);
-  return (
-    <>
-      <div className="h-screen w-screen md:w-auto md:h-auto flex justify-center items-center bg-gray-100">
-        {chatActive ? (
-          <ChatPage socket={socket} username={username} />
-        ) : (
-          <div className="w-screen h-screen flex justify-center items-center gap-2">
-            <input
-              type="text"
-              name=""
-              id=""
-              value={username}
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              className="text-center px-3 py-2 outline-none border-2 rounded-md"
-              placeholder="Enter Username"
-            />
-            <Button
-              type="submit"
-              onClick={() => {
-                !username == "" && setChatActive(true);
-              }}
-              className="bg-gray-500 text-white px-3 py-2 rounded-md"
-            >
-              Start Chat
-            </Button>
-          </div>
-        )}
-      </div>
-    </>
-  );
+	const [username, setUsername] = useState("");
+	const [room, setRoom] = useState("");
+	const [chatActive, setChatActive] = useState(false);
+	return (
+		<>
+			<div className="h-screen w-screen md:w-auto md:h-auto flex justify-center items-center bg-gray-100">
+				{chatActive ? (
+					<ChatPage socket={socket} username={username} room={room} />
+				) : (
+					<div className="w-screen h-screen flex flex-col justify-center items-center gap-2">
+						<form
+							className="flex flex-col gap-1 border-2"
+							onSubmit={() => {
+								!username == "" && !room == "" && setChatActive(true);
+							}}
+						>
+							<input
+								type="text"
+								name=""
+								id=""
+								value={username}
+								onChange={(e) => {
+									setUsername(e.target.value);
+								}}
+								required
+								className="text-center px-3 py-2 outline-none border-2 rounded-md"
+								placeholder="Username"
+							/>
+							<input
+								type="text"
+								name=""
+								id=""
+								value={room}
+								onChange={(e) => {
+									setRoom(e.target.value);
+								}}
+								required
+								className="text-center px-3 py-2 outline-none border-2 rounded-md"
+								placeholder="Room"
+							/>
+							<Button type="submit" className="bg-gray-500 text-white px-3 py-2 rounded-md">
+								Start Chat
+							</Button>
+						</form>
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default HomePage;
