518a1b5e6646101962e8ab815726de1ce17be07b
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-04-19 14:08:02 +0530

    Fix-Core>>Resposiveness chatbox 🚑

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -4,7 +4,7 @@ import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 export default function ChatBox({ messages, username }) {
   return (
     <ScrollArea
-      className="md:h-[60vh] h-[80vh] rounded-md border p-4 "
+      className="md:h-[60vh] h-[70vh] rounded-md border p-4 "
       type="scroll"
     >
       <div className="p-2">
