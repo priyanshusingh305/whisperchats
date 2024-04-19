4b202a4fced851e20192c6154e671b8a9180263e
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-04-19 17:18:11 +0530

    feat-core<chatbox>: style ✨

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -4,7 +4,7 @@ import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 export default function ChatBox({ messages, username }) {
   return (
     <ScrollArea
-      className="md:h-[60vh] h-[70vh] rounded-md border p-4 "
+      className="md:h-[60vh] h-[70vh] rounded-md border p-4 shadow-inner bg-white"
       type="scroll"
     >
       <div className="p-2">
