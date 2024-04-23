1feac36d2af7dcb2993ebd1068b75ef37e4bb3ad
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-04-23 12:34:15 +0530

    initial Setup for build

---
+++ client-2/src/app/dist/page.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+function Home() {
+    return (React.createElement("div", { className: "flex min-h-screen flex-col items-center justify-center p-24" },
+        React.createElement("h1", null, "whisperchats")));
+}
+exports["default"] = Home;
--- client-2/src/app/page.tsx
+++ client-2/src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div>chat app</div>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>whisperchats</h1>
+       
+    </div>
   );
 }
